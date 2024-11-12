7728bf81295ff09403345f626166375269e01e3a
Ndakambani <jamessigauke11@gmail.com>
2024-11-12 15:11:17 +0200

    Update set.cjs

--- set.cjs
+++ set.cjs
@@ -2,7 +2,7 @@ const fs = require("fs");
 require("dotenv").config();
 
 const config = {
-  SESSION_ID: process.env.SESSION_ID || "Gifted~V7mQJvEm",
+  SESSION_ID: process.env.SESSION_ID || "Gifted~wYJWUHnU",
   PREFIX: process.env.PREFIX || '+',
   AUTOLIKE_EMOJI: process.env.AUTOLIKE_EMOJI || '💚', // For liking status updates(stories)
   AUTO_READ_STATUS: process.env.AUTO_READ_STATUS !== undefined ? process.env.AUTO_READ_STATUS === 'true' : true,
